f26c7af0dccfdfafd50753fc7bd4abc6fef766a5
Ivan <van.v2010@yandex.ru>
2022-05-30 00:34:31 +0300

    new content

--- MATH_GAME.py
+++ MATH_GAME.py
@@ -1,7 +1,7 @@
 import math_progr
 import calculator
 import join_division
-
+import prime_number
 
 name = input('Введите ваше имя: ')
 print(f'Привет, {name}!\nТебе нужно пройти математическую игру.\nОна состоит из 4 мини игр, если ты даешь правильный ответ, то получаешь один балл, если не правильный теряешь набранные.\nКогда вы наберете 3 очка, то сможете приступить к следующей мини игре!\nУдачи!')
@@ -9,6 +9,7 @@ calculator.calculate_line_result()
 print('Молодец, приступай к следующей мини-игре!\n')
 math_progr.math_progrission_result()
 print('Молодец, приступай к следующей мини-игре!\n')
-join_division.division_result
+join_division.division_result()
 print('Молодец, приступай к следующей мини-игре!\n')
+prime_number.prime_number_result()
 print('Замечательно, вы прошли игру, возьми с полки пирожок, ты его заслужил!!!!')
--- prime_number.py
+++ prime_number.py
@@ -1,14 +1,15 @@
+from ntpath import join
 from random import randint
 
 
 def prime_number():
-    line = sorted(set([randint(1, 10) for i in range(10)]))
+    line = sorted(set(randint(1, 20) for i in range(15)))
     #print(line)
     z = ''
     for i in line:
         j = 0
         #print(i)
-        for a in range(10):
+        for a in range(20):
             #print(a)
             if i % (int(a) + 1) == 0:
                 j += 1
@@ -16,4 +17,25 @@ def prime_number():
         if j <= 2:
             z += f'{str(i)}, '
     return (line, z[:-2])
-print(prime_number())
+
+#print(prime_number())
+def prime_number_result():
+    """mini-game vith prime number, you need write prime number on line"""
+    i = 0  # обнуляем счетчик итераций(очков набранных пользователем при прохождение игры)
+    while i < 3:  # пользователь должен набрать 3 очка в этой игре
+        line, z = prime_number()  # распаковываем функцию                            
+        user_line = input(f'Перед вами ряд чисел: {(" ".join(map(str,line)))}\nЗапишите какие из них являются простыми, после каждого записаного числа ставьте запятую и пробел: ')
+        try:
+            if user_line == z:
+                i += 1
+                print(f'Молодец!\nКоличество набранных очков: {i}.')
+            else: # если не совпадает, то обнуляется 
+                i = 0
+                print(f'Попробуй еще раз!\nКоличество набранных очков: {i}.')
+        except ValueError: # блок работающий, когда вводишь текст вместо чисел
+            i = 0 
+            print('Ошибка ввода, попробуй еще раз!')
+        if i == 3:
+            print('Поздравляю, вы успешно прошли мини-игру!!!')
+if __name__ == '__main__':
+    prime_number_result()
